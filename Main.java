9fe78fe6fde8df14272809b0b521fc1c0b33b42f
hkrishg <109535284+hkrishg@users.noreply.github.com>
2022-10-11 01:06:14 +0000

    prob stmt added

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
 
+//   Given an array of positive numbers and a positive number ‘S’, find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0, if no such subarray exists.
+
+// Example 1:
+
+// Input: [2, 1, 5, 2, 3, 2], S=7 
+// Output: 2
+// Explanation: The smallest subarray with a sum great than or equal to '7' is [5, 2].
+// Example 2:
+
+// Input: [2, 1, 5, 2, 8], S=7 
+// Output: 1
+// Explanation: The smallest subarray with a sum greater than or equal to '7' is [8].
+
   private static int smallestSubArrayWithGivenSum(int sum, int[] arr) {
     int result = Integer.MAX_VALUE;
     int windowSum = 0;
@@ -18,7 +31,6 @@ class Main {
   }
 
   public static void main(String[] args) {
-    // [3, 4, 1, 1, 6], sum=8 ------- > output = 3 as [1,1,6]
     int result = Main.smallestSubArrayWithGivenSum(8, new int[] { 3, 4, 1, 1, 6 });
     System.out.println("----" + result);
   }
